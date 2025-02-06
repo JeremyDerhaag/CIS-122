161db6458e3d8ede8588aeda9e94c427f9b61c76
JeremyDerhaag <97996829+JeremyDerhaag@users.noreply.github.com>
2025-02-06 14:45:05 -0600

    Add files via upload

---
+++ Discounted inventory.cs
@@ -0,0 +1,67 @@
+﻿// Written by Jeremy Derhaag
+// 02/06/2025
+
+
+namespace Discounted_inventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Prompts and retrieves name from user
+            Console.WriteLine("Hello, what's your name?");
+            string name = Console.ReadLine();
+            
+         
+            
+            // Builds the menu and intake user input
+            Console.WriteLine("The following itens are available:" + "\n" + "1 - Rope" + "\n" + "2 - Torches" + "\n" + "3 - Climbing Equipment" + "\n" + "4 + Clean Water" + "\n"
+                + "5 - Machete" + "\n" + "6 - Canoe" + "\n" + "7 - Food Supplies" + "\n" + "What number do you want to see the price of?");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+           
+            // sets default prices for items
+            double ropePrice = 1;
+            double tourchesPrice = 15;
+            double climbingEquipment = 10;
+            double cleanWaterPrice = 1;
+            double machetePrice = 20;
+            double canoePrice = 40;
+            double foodSuppliesPrice = 2;
+
+
+           // Adds half off discount for matching names
+            if (name == "Jeremy")
+            {
+                ropePrice /= 2;
+                tourchesPrice /= 2;
+                climbingEquipment /= 2;
+                machetePrice /= 2;
+                canoePrice /= 2;
+                foodSuppliesPrice /= 2;
+                cleanWaterPrice /= 2;
+                
+            }
+
+          
+            
+            
+            // switch to decide output
+            string response = choice switch
+            {
+                1 => "Rope Cost " + ropePrice + " gold.",
+                2 => "2 Torches cost " + tourchesPrice + " gold.",
+                3 => "Climbing equipment cost " + climbingEquipment + " gold.",
+                4 => "Clean water cost " + cleanWaterPrice + " gold.",
+                5 => "A Machete costs " + machetePrice +  " gold.",
+                6 => "A canoe costs " + canoePrice + " gold.",
+                7 => "Food supplies cost " + foodSuppliesPrice + " gold."
+            };
+
+            // output
+            Console.WriteLine(response);
+
+
+        }
+    }
+}
---
+++ WatchTower.cs
@@ -0,0 +1,71 @@
+﻿// Written by Jeremy Derhaag
+// 02/06/2025
+
+
+using System.Data.Common;
+
+namespace Hw_2
+{
+    internal class WatchTower
+    {
+        static void Main(string[] args)
+        {
+           // Prompting and reading input
+            Console.WriteLine("X Value: ");
+            string xText = Console.ReadLine();
+
+            Console.WriteLine("Y Value: ");
+            string yText = Console.ReadLine();
+
+            // Converting data types
+            int x = Convert.ToInt32(xText);
+            int y = Convert.ToInt32(yText);
+
+            // creating strings to assign values to
+            string row = "";
+            string col = "";
+            
+            // Assigning values to the strings
+            if (x > 0)
+            {
+                col = "East";
+            }
+            else if (x < 0)
+            {
+                col = "West";
+            }
+            else
+            {
+                col = "";
+            }
+
+            if (y > 0)
+            {
+                row = "North";
+            }
+            else if (y < 0)
+            {
+                row = "South";
+            }
+            else
+            {
+                if (x == 0)
+                {
+                    row = "Here";
+                }
+                else
+                {
+                    row = "";
+                }
+                
+            }
+
+
+            // combining the strings
+            string location = row + col;
+            // output
+            Console.WriteLine("The Enemy is " + location + "!");
+
+        }
+    }
+}
